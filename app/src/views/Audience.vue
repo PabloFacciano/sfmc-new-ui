e044ee6ae4f811c6a1cd69f82b4f99733630a53f
Pablo Facciano <pablo.facciano@globant.com>
2022-05-08 14:55:13 -0300

    new view: Audience

--- app/src/views/Audience.vue
+++ app/src/views/Audience.vue
@@ -1,16 +1,8 @@
 <template>
-  <div>
+  <div class="p-3">
     Audience.vue
-    <ul>
-      <li><router-link class="nav-link rounded ms-2" to="./contacts">Contacts</router-link></li>
-      <li><router-link class="nav-link rounded ms-2" to="./dataextensions">Data Extensions</router-link></li>
-      <li><router-link class="nav-link rounded ms-2" to="./dataviews">Data Views</router-link></li>
-      <li><router-link class="nav-link rounded ms-2" to="./subscription">Subscription</router-link></li>
-      <li><router-link class="nav-link rounded ms-2" to="./classic">Classic</router-link></li>
-      <li>----</li>
-      <li><router-link class="nav-link rounded ms-2" to="../">Go back</router-link></li>
-    </ul>
     <hr>
+    <router-view></router-view>
   </div>
 </template>
 
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,14 +1,7 @@
 <template>
-  <div>
+  <div class="p-3">
     <h1>Home.vue</h1>
     <p>Nothing to see here yet!</p>
-    <hr>
-    <p>Other sections:</p>
-    <ul>
-      <li><router-link class="nav-link rounded ms-2" to="/audience">Audience</router-link></li>
-      <li><router-link class="nav-link rounded ms-2" to="/automation">Automation</router-link></li>
-      <li><router-link class="nav-link rounded ms-2" to="/about">About</router-link></li>
-    </ul>
     <!-- iframe src="" class="w-100 h-100" /-->
   </div>
 </template>
