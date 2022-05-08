cd9227e6b84d690c29a3682e3f69682517075b26
Pablo Facciano <pablo.facciano@globant.com>
2022-05-07 22:22:55 -0300

    new app version

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,6 +2,13 @@
   <div>
     <h1>Home.vue</h1>
     <p>Nothing to see here yet!</p>
+    <hr>
+    <p>Other sections:</p>
+    <ul>
+      <li><router-link class="nav-link rounded ms-2" to="/audience">Audience</router-link></li>
+      <li><router-link class="nav-link rounded ms-2" to="/automation">Automation</router-link></li>
+      <li><router-link class="nav-link rounded ms-2" to="/about">About</router-link></li>
+    </ul>
     <!-- iframe src="" class="w-100 h-100" /-->
   </div>
 </template>
